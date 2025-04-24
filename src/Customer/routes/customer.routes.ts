0af8ad3c29b92050d60a86a9518199f0cbf5a6b0
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-04-24 16:21:43 -0400

    feat(customerRoutes): creating route to save user

---
+++ src/Customer/routes/customer.routes.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { controller } from '..'
+
+const router = express.Router()
+
+router.post('/customer', async(req,res) => {
+    controller.save(req,res)
+})
+
+export {router}
