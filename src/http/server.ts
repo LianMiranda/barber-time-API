dcf69776077efb30f81cacfa2da873acd7460e73
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-04-23 21:24:51 -0400

    feat: initial project scope

---
+++ src/http/server.ts
@@ -0,0 +1,7 @@
+import { app } from "..";
+import { config } from "dotenv";
+config()
+
+const port: string | undefined = process.env.PORT
+
+app.listen(port, () => console.log(`App is running in port ${port}`))
