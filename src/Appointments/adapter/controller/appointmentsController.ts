bb230b331526596e4502b0e2e7777a572e74a27d
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-04-29 17:15:26 -0400

    feat: appointmentsController

---
+++ src/Appointments/adapter/controller/appointmentsController.ts
@@ -0,0 +1,118 @@
+import { AppError } from "../../../shared/errors/appError";
+import { AppointmentUseCaseInterface } from "../../use_case/appointmentsUseCaseInterface";
+import { Request, Response } from "express";
+class AppointmentController {
+  constructor(private appointmentUseCase: AppointmentUseCaseInterface) {}
+
+  async create(req: Request, res: Response) {
+    const data = req.body;
+    try {
+        await this.appointmentUseCase.create(data);
+        res.status(201).json({ message: "Appointment created successfully" });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async findAll(req: Request, res: Response) {
+    try {
+      const appointments = await this.appointmentUseCase.findAll();
+      res.status(200).json({ appointments });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async findById(req: Request, res: Response) {
+    const id = req.params.id;
+    try {
+      const appointment = await this.appointmentUseCase.findById(id);
+      res.status(200).json({ appointment });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+  async findByCustomerId(req: Request, res: Response) {
+    const customerId = req.params.customerId;
+    try {
+      const appointments = await this.appointmentUseCase.findByCustomerId(customerId);
+      res.status(200).json({ appointments });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async findByServiceId(req: Request, res: Response) {
+    const serviceId = req.params.serviceId;
+    try {
+      const appointments = await this.appointmentUseCase.findByServiceId(serviceId);
+      res.status(200).json({ appointments });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+  async findByDate(req: Request, res: Response) {
+    const schedule_at = req.body.schedule_at;
+    try {
+      const appointments = await this.appointmentUseCase.findByDate(schedule_at);
+      res.status(200).json({ appointments });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+      await this.appointmentUseCase.update(id, data);
+      res.status(200).json({ message: "Appointment updated successfully" });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    const id = req.params.id;
+    try {
+      await this.appointmentUseCase.delete(id);
+      res.status(200).json({ message: "Appointment deleted successfully" });
+    } catch (err) {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
+
+export { AppointmentController };
